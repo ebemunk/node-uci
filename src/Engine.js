6b3a410bf12fd855a29d38cc895f33df427b71fa
ebemunk <ebemunk@gmail.com>
2016-07-24 02:13:00 -0700

    refactor out bestmove parsing

--- src/Engine.js
+++ src/Engine.js
@@ -171,7 +171,24 @@ function parseInfo(line) {
 		}
 	})
 	log('info', info, EOL)
+	if( _.isEmpty(info) ) {
+		console.warn('parseInfo() received line it couldnt parse: ', line)
+		return
+	}
 	return info
+}
+
+//parse "bestmove" command
+function parseBestmove(line) {
+	const bestmove = REGEX.bestmove.exec(line)
+	if( ! bestmove || ! bestmove[1] ) return
+	const parsed = {
+		bestmove: bestmove[1]
+	}
+	if( bestmove[2] ) {
+		parsed.ponder = bestmove[2]
+	}
+	return parsed
 }
 
 export default class Engine {
