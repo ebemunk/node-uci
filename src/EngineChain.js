18b12da693f861b669e91fcc98865d4f5bb114bf
ebemunk <ebemunk@gmail.com>
2016-08-25 21:34:24 -0700

    EngineChain renovations

--- src/EngineChain.js
+++ src/EngineChain.js
@@ -2,6 +2,8 @@ import Promise from 'bluebird'
 import debug from 'debug'
 import {last} from 'lodash'
 
+import Engine from './Engine'
+
 const log = debug('uci:EngineChain')
 
 const CHAINABLE = [
@@ -16,29 +18,36 @@ const CHAINABLE = [
 
 export default class EngineChain {
 	constructor(engine) {
+		if( ! engine || ! (engine instanceof Engine) )
+			throw new Error('EngineChain requires a valid Engine.')
+		//init
 		log('chain init')
 		this._engine = engine
 		this._queue = []
-
-		CHAINABLE.forEach(fn => {
-			this[fn] = this.chain(fn)
+		//construct chain functions
+		CHAINABLE.forEach(funcName => {
+			this[funcName] = this.chain(funcName)
 		})
 	}
 
 	//returns a function which puts the Engine call and args in the queue
-	chain(fn) {
+	chain(funcName) {
 		const self = this
 		return function () {
-			this._queue.push([::self._engine[fn], [...arguments]])
-			return this
+			this._queue.push([::self._engine[funcName], [...arguments]])
+			if( funcName === 'go' ) {
+				return this.exec()
+			} else {
+				return this
+			}
 		}
 	}
 
-	async commit() {
-		const results = await Promise.mapSeries(this._queue, ([fn, params]) => {
+	async exec() {
+		const results = await Promise.map(this._queue, ([fn, params]) => {
 			return fn(...params)
-		})
-
+		}, {concurrency: 1})
+		this._queue = []
 		return last(results)
 	}
 }
