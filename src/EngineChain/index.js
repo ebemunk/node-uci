e0511815bfd2e76053e0b6d9cef9095a9da82418
ebemunk <terchon@gmail.com>
2016-11-22 01:16:52 -0800

    rollup works

--- src/EngineChain/index.js
+++ src/EngineChain/index.js
@@ -87,7 +87,7 @@ export default class EngineChain {
 	 * @return {any} - last return value from the queued {@link Engine} method
 	 */
 	async exec() {
-		const results = Promise.mapSeries(this._queue, ([fn, params]) => {
+		const results = await Promise.mapSeries(this._queue, ([fn, params]) => {
 			return fn(...params)
 		})
 		this._queue = []
