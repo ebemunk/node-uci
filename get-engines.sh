48882fb7d18df09a4f9ba8787b633472aa34de2b
ebemunk <ebemunk@gmail.com>
2018-09-25 00:50:19 -0700

    hurray

--- get-engines.sh
+++ get-engines.sh
@@ -3,5 +3,3 @@ curl https://stockfish.s3.amazonaws.com/stockfish-9-linux.zip -o tmp/stockfish-9
 unzip tmp/stockfish-9-linux.zip -d tmp/
 mkdir engines
 mv tmp/stockfish-9-linux/Linux/stockfish-9-bmi2 engines/stockfish-9-64
-ls -lha engines
-./engines/stockfish-9-64
