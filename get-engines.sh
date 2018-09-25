9d3258338cf333436324c8872c3ecd2bad6d5fc6
ebemunk <ebemunk@gmail.com>
2018-09-24 23:12:13 -0700

    mkdir engines

--- get-engines.sh
+++ get-engines.sh
@@ -1,4 +1,5 @@
 mkdir tmp
 curl https://stockfish.s3.amazonaws.com/stockfish-9-linux.zip -o tmp/stockfish-9-linux.zip
 unzip tmp/stockfish-9-linux.zip -d tmp/
+mkdir engines
 mv tmp/stockfish-9-linux/Linux/stockfish-9-64 engines/
